1e9c9a80998c3220902080f047bb139ee9bb0a37
mehgoss <60152334+mehgoss@users.noreply.github.com>
2025-03-28 08:08:24 +0200

    Update App.py

--- App.py
+++ App.py
@@ -54,6 +54,7 @@ def long_running_task(logger,bot):
             risk_per_trade=0.02,
             log=logger,
             telegram_bot=bot
+            api=api
         )
         
         # Log start time
